19002b364daf75313719c40e690b723cf5d0fc5a
AiramLD <airamluisdiaz629@gmail.com>
2023-10-20 19:03:07 +0100

    Instalacion básica

---
+++ Player.js
@@ -0,0 +1,13 @@
+class Player{
+    name;
+
+    constructor(name){
+        this.name = name;
+
+    }
+
+}
+
+//ESTO SON PRUEBAS (ELIMINAR A PARTIR DE AQUI)
+let player1 = new Player("Juan");
+console.log(player1);
